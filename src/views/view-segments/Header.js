2047b7391dcd610bd0d4d4d66d8b2a2b64f7efca
Hokusai12 <hokusai2003@gmail.com>
2023-07-18 19:42:33 -0500

    Added emailjs

--- src/views/view-segments/Header.js
+++ src/views/view-segments/Header.js
@@ -7,10 +7,10 @@ function Header({updateView}) {
                     <a className="navbar-brand" href="#">
                         <img src="mr-crafts-imgs/mr-crafts-logo.svg" style={{maxHeight: "50px"}} className="img-fluid" alt="Mr. Craft's Handyman Services Logo"/> 
                     </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                <div class="container">
+                <div className="container">
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav fs-5">
                             <li className="nav-item mx-5">
